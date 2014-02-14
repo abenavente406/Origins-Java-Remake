645a96cffeecd168c99fc9fb3d081a408ad05e0e
abenavente406@gmail.com <abenavente406@gmail.com>
2014-02-14 15:19:29 -0500

    Player test now uses a test level instead of random tiles

--- src/com/ambenavente/origins/tests/PlayerTest.java
+++ src/com/ambenavente/origins/tests/PlayerTest.java
@@ -18,6 +18,8 @@
 package com.ambenavente.origins.tests;
 
 import com.ambenavente.origins.gameplay.entities.player.Player;
+import com.ambenavente.origins.gameplay.world.json.MapDeserializer;
+import com.ambenavente.origins.gameplay.world.level.TiledMap;
 import com.ambenavente.origins.util.Camera;
 import org.newdawn.slick.*;
 import org.newdawn.slick.geom.Vector2f;
@@ -32,15 +34,9 @@ import java.util.Random;
  */
 public class PlayerTest extends BasicGame {
 
-    private final int DEFAULT_WIDTH         = 100;
-    private final int DEFAULT_HEIGHT        = 100;
-    private final int DEFAULT_TILE_WIDTH    = 32;
-    private final int DEFAULT_TILE_HEIGHT   = 32;
-
-    private Random rand;
     private Camera camera;
     private Player player;
-    private ColorTile[][] tiles;
+    private TiledMap map;
 
     public PlayerTest(String title) {
         super(title);
@@ -48,74 +44,35 @@ public class PlayerTest extends BasicGame {
 
     @Override
     public void init(GameContainer container) throws SlickException {
-        rand    = new Random();
+        map     = new MapDeserializer()
+                     .readFromJsonFile("res/json/test_map_2.json");
         camera  = new Camera(0,
                              0,
                              container.getWidth(),
                              container.getHeight());
-        tiles   = new ColorTile[DEFAULT_HEIGHT][DEFAULT_WIDTH];
         player  = new Player(0, 0);
 
+        camera.setMax(new Vector2f(map.getRealWidth(), map.getRealHeight()));
         camera.setMin(new Vector2f(0, 0));
-
-        for (int y = 0; y < DEFAULT_HEIGHT; y++) {
-            for (int x = 0; x < DEFAULT_WIDTH; x++) {
-                tiles[y][x] = new ColorTile(x,
-                        y,
-                        getRandColor());
-            }
-        }
     }
 
     @Override
     public void update(GameContainer container, int i) throws SlickException {
-        player.update(container, i);
+        player.update(container, map, i);
         camera.setX(player.getX() - camera.getViewWidth() / 2);
         camera.setY(player.getY() - camera.getViewHeight() / 2);
     }
 
     @Override
-    public void render(GameContainer container, Graphics graphics) throws SlickException {
+    public void render(GameContainer container,
+                       Graphics graphics) throws SlickException {
         graphics.pushTransform();
         graphics.translate(-camera.getX(), -camera.getY());
-        graphics.scale(camera.getZoom(), camera.getZoom());
-        int minX = toTileX(camera.getX());
-        int minY = toTileY(camera.getY());
-        int maxX = toTileX(camera.getX()) + toTileX(camera.getViewWidth());
-        int maxY = toTileY(camera.getY()) + toTileY(camera.getViewHeight());
 
-        for (int y = minY; y < maxY + 1; y++) {
-            if (y < DEFAULT_HEIGHT && y >= 0) {
-                for (int x = minX; x < maxX + 1; x++) {
-                    if (x < DEFAULT_WIDTH && x >= 0) {
-                        graphics.setColor(tiles[y][x].getColor());
-                        graphics.fillRect(x * DEFAULT_TILE_WIDTH,
-                                          y * DEFAULT_TILE_HEIGHT,
-                                          DEFAULT_TILE_WIDTH,
-                                          DEFAULT_TILE_HEIGHT);
-                    }
-                }
-            }
-        }
-
+        map.render(camera, graphics);
         player.render(graphics);
 
         graphics.popTransform();
-    }
-
-    private Color getRandColor() {
-        int r = rand.nextInt(256);
-        int g = rand.nextInt(256);
-        int b = rand.nextInt(256);
-        return new Color(r, g, b);
-    }
-
-    private int toTileX(float x) {
-        return (int) (x / DEFAULT_TILE_WIDTH);
-    }
-
-    private int toTileY(float y) {
-        return (int) (y / DEFAULT_TILE_HEIGHT);
     }
 
     public static void main(String[] args) {
