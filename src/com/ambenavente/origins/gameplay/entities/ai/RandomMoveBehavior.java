70d54dd3c1bb7093ab05cf52d443a5c5b19608ab
abenavente406@gmail.com <abenavente406@gmail.com>
2014-02-17 16:14:37 -0500

    Finally implemented the random movement behavior

--- src/com/ambenavente/origins/gameplay/entities/ai/RandomMoveBehavior.java
+++ src/com/ambenavente/origins/gameplay/entities/ai/RandomMoveBehavior.java
@@ -17,8 +17,11 @@
 
 package com.ambenavente.origins.gameplay.entities.ai;
 
+import com.ambenavente.origins.gameplay.entities.Direction;
 import com.ambenavente.origins.gameplay.entities.Entity;
 import com.ambenavente.origins.gameplay.entities.interfaces.Behavior;
+import com.ambenavente.origins.util.MyRandom;
+import org.newdawn.slick.geom.Vector2f;
 
 /**
  * Created with IntelliJ IDEA.
@@ -30,12 +33,66 @@ public class RandomMoveBehavior implements Behavior {
 
     private Entity owner;
 
+    private int timeSinceMove;
+    private int maxTime;
+    private int timeMoving;
+    private int timeOut;
+    private Direction randDirection;
+    private float walkingSpeed;
+    private boolean isMoving;
+
     public RandomMoveBehavior(Entity owner) {
-        this.owner = owner;
+        this.owner          = owner;
+        this.timeSinceMove  = 0;
+        this.timeMoving     = 0;
+        this.timeOut        = 0;
+        this.maxTime        = 0;
+        this.isMoving       = false;
+        this.randDirection  = Direction.getRandDirection();
+        this.walkingSpeed   = owner.getWalkingSpeed();
     }
 
     @Override
     public void behave() {
-        // TODO: Move randomly
+        if (!isMoving) {
+            if (timeSinceMove++ >= timeOut) {
+                isMoving = true;
+                timeMoving = 0;
+                maxTime = MyRandom.getRandBetween(50, 300);
+                timeSinceMove = 0;
+                timeOut = MyRandom.getRandBetween(10, 80);
+                randDirection = Direction.getRandDirection();
+            }
+        } else {
+            moveEntity();
+
+            if (timeMoving++ >= maxTime) {
+                timeSinceMove = 0;
+                isMoving = false;
+            }
+        }
+
+        owner.setIsMoving(isMoving);
+    }
+
+    /**
+     * Moves the entity in a random direction
+     */
+    private void moveEntity() {
+
+        Vector2f amount = new Vector2f(0, 0);
+
+        if (randDirection == Direction.NORTH) {
+            amount.y -= walkingSpeed;
+        } else if (randDirection == Direction.EAST) {
+            amount.x += walkingSpeed;
+        } else if (randDirection == Direction.SOUTH) {
+            amount.y += walkingSpeed;
+        } else {
+            amount.x -= walkingSpeed;
+        }
+
+        owner.setDirection(randDirection);
+        owner.move(amount);
     }
 }
